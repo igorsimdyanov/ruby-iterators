e64bf95868d32fa62b414a415fabb283f84e5475
Igor Simdyanov <igorsimdyanov@gmail.com>
2017-07-18 11:12:17 +0300

    More synonyms

--- collections/reject.rb
+++ collections/reject.rb
@@ -1,1 +1,2 @@
 [1, 2, 3, 4, 5].reject { |x| x.even? }
+[1, 2, 3, 4, 5].delete_if { |x| x.even? }
--- collections/select.rb
+++ collections/select.rb
@@ -1,2 +1,2 @@
 [1, 2, 3, 4, 5].select { |x| x.even? }
-[1, 2, 3, 4, 5].map { |x| x.even? }
+[1, 2, 3, 4, 5].find_all { |x| x.even? }
